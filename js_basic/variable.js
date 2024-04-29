6154a2018eeb8947901191787b99053f8375c269
giyong <giyong-hill@giyongui-MacBookAir.local>
2024-04-30 06:20:24 +0900

    js basic

---
+++ js_basic/variable.js
@@ -0,0 +1,16 @@
+var msg = "Hello";
+msg = "World";
+console.log(msg);
+
+var nan = 'a' / 10;
+console.log(nan);
+
+//null & undefined
+let name;  //값이 할당되지 않음(의도적이지 않게 값을 넣지 않았음)
+console.log(name);
+
+let address = '';  //의도적으로 공백 문자를 넣음
+console.log(address)
+
+let nickname = null   //값이 비어있음(의도적으로 값을 비워둠)
+console.log(nickname);
